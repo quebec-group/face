84d19d2b3aec2556e7c47543c278de02c545fb3b
Callum Ryan <callum@MacBook-Pro-9.local>
2017-02-23 23:14:12 +0000

    Cleanup

--- src/main/java/uk/ac/cam/cl/quebec/face/aws/LambdaCaller.java
+++ src/main/java/uk/ac/cam/cl/quebec/face/aws/LambdaCaller.java
@@ -4,8 +4,6 @@ import com.amazonaws.services.lambda.AWSLambdaClient;
 import com.amazonaws.services.lambda.invoke.LambdaFunction;
 import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
 
-import java.util.Arrays;
-
 public class LambdaCaller {
     private AWSLambdaClient client;
     private EventProcessedLambdaService eventService;
@@ -26,7 +24,7 @@ public class LambdaCaller {
     public LambdaCaller() {
         client = new AWSLambdaClient(CredentialsManager.getCredentials());
         client.setRegion(CredentialsManager.getRegion());
-        
+
         eventService = LambdaInvokerFactory.build(EventProcessedLambdaService.class, client);
         profileService = LambdaInvokerFactory.build(ProfileProcessedLambdaService.class, client);
     }
@@ -38,17 +36,4 @@ public class LambdaCaller {
     public LambdaOutput profileProcessedLambda(ProfileProcessedLambdaInput input) {
         return profileService.run(input);
     }
-
-    public static void main(String[] args) {
-        LambdaCaller caller = new LambdaCaller();
-
-        EventProcessedLambdaInput request = new EventProcessedLambdaInput();
-        request.setEventID("7");
-        request.setMembers(Arrays.asList("userA", "userB", "userC"));
-
-        LambdaOutput result = caller.callEventProcessedLambda(request);
-
-        System.out.println(result);
-    }
-
 }
