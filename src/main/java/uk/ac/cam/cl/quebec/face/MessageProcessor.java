c2cbea727377059f557f6c190a49f36d86435e9b
PML369 <plpub2@gmail.com>
2017-02-28 00:01:31 +0000

    Some refactoring of MessageProcessor

--- src/main/java/uk/ac/cam/cl/quebec/face/MessageProcessor.java
+++ src/main/java/uk/ac/cam/cl/quebec/face/MessageProcessor.java
@@ -10,7 +10,6 @@ import uk.ac.cam.cl.quebec.face.aws.EventProcessedLambdaInput;
 import uk.ac.cam.cl.quebec.face.aws.LambdaCaller;
 import uk.ac.cam.cl.quebec.face.aws.ProfileProcessedLambdaInput;
 import uk.ac.cam.cl.quebec.face.config.Config;
-import uk.ac.cam.cl.quebec.face.exceptions.BadImageFormatException;
 import uk.ac.cam.cl.quebec.face.exceptions.StorageException;
 import uk.ac.cam.cl.quebec.face.exceptions.VideoLoadException;
 import uk.ac.cam.cl.quebec.face.exceptions.QuebecException;
@@ -23,7 +22,6 @@ import uk.ac.cam.cl.quebec.face.aws.S3Manager;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -42,21 +40,21 @@ public class MessageProcessor implements MessageVisitor
     private static final int minDetectHeight = 30;
     private static final int minNumberOfFramesMatching = 5;
 
-    private S3Manager s3Downloader;
+    private S3Manager s3Manager;
     private Config config;
 
     private static Mat singletonZeroLabel = new Mat(1, 1, CvType.CV_32SC1, new Scalar(0));
 
     public MessageProcessor(Config config, S3Manager downloader) {
         this.config = config;
-        s3Downloader = downloader;
+        s3Manager = downloader;
     }
 
     public void accept(TrainOnVideoMessage msg) throws QuebecException
     {
         System.err.println("Processing TrainOnVideoMessage: " + Integer.toString(msg.getVideoId()));
         // Fetch image from S3
-        String videoPath = s3Downloader.downloadFile(msg);
+        String videoPath = s3Manager.downloadFile(msg);
 
         // Load it into memory - all our OpenCV operations operate on greyscale images
         Mat imageInput = new Mat();
@@ -78,25 +76,10 @@ public class MessageProcessor implements MessageVisitor
 
         // TODO: extract a profile picture from a video and upload to s3 here
         // For now, use the frame we were working with
-        File tmpDir = DirectoryStructure.getOrMakeTempDirectory(config);
-        File profilePhoto;
-        try {
-            profilePhoto = File.createTempFile("profile", ".jpg", tmpDir);
-        }
-        catch (IOException e) {
-            throw new StorageException("Could not create temp file for profile photo", e);
-        }
-        Imgcodecs.imwrite(profilePhoto.getAbsolutePath(), imageInput);
-        String s3FileName = "protected/" + msg.getUserId() + "/photos/profilePhoto.jpg";
-        s3Downloader.uploadFile(profilePhoto, s3FileName);
-        profilePhoto.delete();
+        String s3FileName = uploadProfilePhoto(msg.getUserId(), imageInput);
 
         // Send profile picture back to Amazon
-        ProfileProcessedLambdaInput results = new ProfileProcessedLambdaInput();
-        results.setUserID(msg.getUserId());
-        results.setS3ID(s3FileName);
-        LambdaCaller c = new LambdaCaller(config);
-        c.callProfileProcessedLambda(results);
+        sendTrainingResultsToLambda(msg.getUserId(), s3FileName);
     }
 
     public void accept(ProcessVideoMessage msg) throws QuebecException
@@ -104,7 +87,7 @@ public class MessageProcessor implements MessageVisitor
         System.err.println("Processing ProcessVideoMessage: " + Integer.toString(msg.getVideoId()));
 
         // Fetch video from s3
-        String videoFileName = s3Downloader.downloadFile(msg);
+        String videoFileName = s3Manager.downloadFile(msg);
 
         // We create a recogniser here because creation has some interesting failure modes,
         // which will make it pointless to perform the face detection step
@@ -163,11 +146,7 @@ public class MessageProcessor implements MessageVisitor
         // TODO: Store results of processing persistently on disk
 
         // Upload results of processing to Amazon
-        EventProcessedLambdaInput results = new EventProcessedLambdaInput();
-        results.setEventID(msg.getEventId());
-        results.setMembers(usersInVideo);
-        LambdaCaller c = new LambdaCaller(config);
-        c.callEventProcessedLambda(results);
+        sendEventResultsToLambda(msg.getEventId(), usersInVideo);
     }
 
     private Map.Entry<Integer, Double> getBestMatch(Mat face, FaceRecognizer recognizer) {
@@ -175,5 +154,39 @@ public class MessageProcessor implements MessageVisitor
         double confidence[] = new double[1];
         recognizer.predict(face, labs, confidence);
         return new AbstractMap.SimpleEntry<>(labs[0], confidence[0]);
+    }
+
+    private String uploadProfilePhoto(String userId, Mat photo) throws StorageException {
+        File tmpDir = DirectoryStructure.getOrMakeTempDirectory(config);
+        File profilePhoto;
+        try {
+            profilePhoto = File.createTempFile("profile", ".jpg", tmpDir);
+        }
+        catch (IOException e) {
+            throw new StorageException("Could not create temp file for profile photo", e);
+        }
+
+        Imgcodecs.imwrite(profilePhoto.getAbsolutePath(), photo);
+        String s3FileName = "protected/" + userId + "/photos/profilePhoto.jpg";
+        s3Manager.uploadFile(profilePhoto, s3FileName);
+
+        profilePhoto.delete();
+        return s3FileName;
+    }
+
+    private void sendTrainingResultsToLambda(String userId, String s3FileName) {
+        ProfileProcessedLambdaInput results = new ProfileProcessedLambdaInput();
+        results.setUserID(userId);
+        results.setS3ID(s3FileName);
+        LambdaCaller c = new LambdaCaller(config);
+        c.callProfileProcessedLambda(results);
+    }
+
+    private void sendEventResultsToLambda(String eventId, List<String> usersInVideo) {
+        EventProcessedLambdaInput results = new EventProcessedLambdaInput();
+        results.setEventID(eventId);
+        results.setMembers(usersInVideo);
+        LambdaCaller c = new LambdaCaller(config);
+        c.callEventProcessedLambda(results);
     }
 }
